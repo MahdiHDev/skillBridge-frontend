9fd141931956c11926d399a51a95d49e089c9918
Mahdi Hussain <mahdi.hussain.work@gmail.com>
2026-03-20 03:36:04 +0600

    added review section

---
+++ src/components/modules/authentication/login-form.tsx
@@ -0,0 +1,165 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import {
+    Field,
+    FieldDescription,
+    FieldError,
+    FieldGroup,
+    FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
+import { cn } from "@/lib/utils";
+import { useForm } from "@tanstack/react-form";
+import { toast } from "sonner";
+import * as z from "zod";
+
+const formSchema = z.object({
+    password: z.string().min(8, "Minimum length is 8 characters"),
+    email: z.email(),
+});
+
+export function LoginForm({
+    className,
+    ...props
+}: React.ComponentProps<"div">) {
+    const form = useForm({
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+        validators: {
+            onSubmit: formSchema,
+        },
+        onSubmit: async ({ value }) => {
+            const toastId = toast.loading("Logging in");
+            try {
+                const { data, error } = await authClient.signIn.email(value);
+
+                if (error) {
+                    toast.error(error.message, { id: toastId });
+                    return;
+                }
+
+                toast.success("User Logged in Successfully", { id: toastId });
+            } catch (error) {
+                toast.error("Something went wrong, please try again.", {
+                    id: toastId,
+                });
+                console.error(error);
+            }
+        },
+    });
+
+    return (
+        <div className={cn("flex flex-col gap-6", className)} {...props}>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Login to your account</CardTitle>
+                    <CardDescription>
+                        Enter your email below to login to your account
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <form
+                        id="login-form"
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            form.handleSubmit();
+                        }}
+                    >
+                        <FieldGroup>
+                            <form.Field
+                                name="email"
+                                children={(field) => {
+                                    const isInvalid =
+                                        field.state.meta.isTouched &&
+                                        !field.state.meta.isValid;
+                                    return (
+                                        <Field>
+                                            <FieldLabel htmlFor={field.name}>
+                                                Email
+                                            </FieldLabel>
+                                            <Input
+                                                type="email"
+                                                id={field.name}
+                                                name={field.name}
+                                                value={field.state.value}
+                                                onChange={(e) =>
+                                                    field.handleChange(
+                                                        e.target.value,
+                                                    )
+                                                }
+                                            />
+                                            {isInvalid && (
+                                                <FieldError
+                                                    errors={
+                                                        field.state.meta.errors
+                                                    }
+                                                />
+                                            )}
+                                        </Field>
+                                    );
+                                }}
+                            />
+                            <form.Field
+                                name="password"
+                                children={(field) => {
+                                    const isInvalid =
+                                        field.state.meta.isTouched &&
+                                        !field.state.meta.isValid;
+                                    return (
+                                        <Field>
+                                            <FieldLabel htmlFor={field.name}>
+                                                Password
+                                            </FieldLabel>
+                                            <Input
+                                                type="password"
+                                                id={field.name}
+                                                name={field.name}
+                                                value={field.state.value}
+                                                onChange={(e) =>
+                                                    field.handleChange(
+                                                        e.target.value,
+                                                    )
+                                                }
+                                            />
+                                            {isInvalid && (
+                                                <FieldError
+                                                    errors={
+                                                        field.state.meta.errors
+                                                    }
+                                                />
+                                            )}
+                                        </Field>
+                                    );
+                                }}
+                            />
+
+                            <Field>
+                                <Button form="login-form" type="submit">
+                                    Login
+                                </Button>
+                                {/* <Button variant="outline" type="button">
+                  Login with Google
+                </Button> */}
+                                <FieldDescription className="text-center">
+                                    Don&apos;t have an account?{" "}
+                                    <a href="/signup">Sign up</a>
+                                </FieldDescription>
+                            </Field>
+                        </FieldGroup>
+                    </form>
+                </CardContent>
+            </Card>
+        </div>
+    );
+}
