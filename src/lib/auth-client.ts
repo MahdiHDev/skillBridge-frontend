e16c3ca03e6acda1319774b84a23249d061e6da1
Mahdi Hussain <mahdi.hussain.work@gmail.com>
2026-04-24 18:21:52 +0600

    feat: update authClient to use NEXT_PUBLIC_BACKEND_URL for baseURL

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"; // make sure to import from better-auth/react
 
 export const authClient = createAuthClient({
-    baseURL: process.env.AUTH_URL,
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     // baseURL: "http://localhost:5000/",
 });
